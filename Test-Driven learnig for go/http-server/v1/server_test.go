e35c7bfa8f57b6a8fdc3baa27c37c90376c10bf6
jackhyx <hyx_312@foxmail.com>
2022-05-09 01:42:24 +0800

    go 学习笔记

--- Test-Driven learnig for go/http-server/v1/server_test.go
+++ Test-Driven learnig for go/http-server/v1/server_test.go
@@ -1,17 +1,10 @@
-package appprogram
+package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
-
-// PlayerServer currently returns Hello, world given _any_ request.
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
-}
 
 func TestGETPlayers(t *testing.T) {
 	request, _ := http.NewRequest(http.MethodGet, "/", nil)
@@ -28,11 +21,4 @@ func TestGETPlayers(t *testing.T) {
 		}
 	})
 
-}
-
-func main() {
-	handler := http.HandlerFunc(PlayerServer)
-	if err := http.ListenAndServe(":5000", handler); err != nil {
-		log.Fatalf("could not listen on port 5000 %v", err)
-	}
 }
